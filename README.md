351873302f52142df111af9962a83fe73384fa2d
Mathias Garbe <me@mathias-garbe.de>
2013-02-13 21:46:56 +0100

    Flask.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 coffeeadmin
 ===========
+
+To init all submodules:
+```git submodule init
+git submodule update
+cd coffeeserver/
+git submodule init
+git submodule update```
---
+++ coffeeadmin.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+from flask import Flask, render_template
+from flask.ext.bootstrap import Bootstrap
+sys.path.insert(0, "coffeeserver/")
+import coffeeserver
+
+app = Flask(__name__)
+Bootstrap(app)
+app.config['BOOTSTRAP_USE_CDN'] = True
+
+p = coffeeserver.load_payment()
+
+@app.route('/')
+def index():
+    p.getItems()
+    return render_template('test.html')
+
+
+@app.route('/breakme/')
+def breakme():
+    return render_template('test.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
