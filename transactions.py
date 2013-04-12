c2ae06be1f3c20047cf969589e49d18fc6050dd9
Mathias Garbe <me@mathias-garbe.de>
2013-04-12 09:05:59 +0200

    Basic transaction view

--- transactions.py
+++ transactions.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 from utils import *
+from payment import *
 
 transactions = Blueprint('transactions', __name__, template_folder='templates')
 
 @transactions.route('/transactions/')
 @login_required
 def transactionsf():
-    return render('placeholder.html')
+    t = current_app.payment.session.query(Transaction).order_by(Transaction.time.desc()).all()
+    return render('transactions.html', transactions=t)
